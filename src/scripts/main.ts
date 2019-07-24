deb5698d44827a357f171311515cfac82fd6d99e
Joe Barnett <joebarnett876@gmail.com>
2019-07-24 17:27:44 +0100

    remove vestigial grid-drawing mode stuff

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,67 +1,10 @@
 import drawing from './drawing';
 import Snap from 'snapsvg';
-import { relativeCoords } from './mouse.js';
 import Graph from './graph';
-import { INode } from './types';
 export default Snap('#surface');
-
-const noOp = () => {};
 
 function drawSquareGrid() {
   drawing.graph = new Graph();
-}
-
-function makeDraggable(node: INode) {
-  node.snapObject.drag(makeDragHandler(node), noOp, redrawKnot);
-}
-function makeAllNodesDraggable() {
-  drawing.frame.nodes.forEach((node) => {
-    makeDraggable(node);
-  });
-}
-function makeDragHandler(node: INode) {
-  return (e)=> {
-    // change node position
-    [node.x, node.y] = relativeCoords(e);
-    // re-draw whole frame
-    drawing.frame.remove();
-    drawing.frame.draw();
-    // but now we have new snap objs without listeners attached
-    // so add listeners again
-    makeAllNodesDraggable();
-    // uncomment the line below to make freeform 10x as fun
-    redrawKnot();
-  };
-}
-function redrawKnot() {
-  drawing.knot.remove();
-  drawing.drawKnot();
-  makeAllNodesDraggable();
-}
-function startFreeformMode() {
-  if (drawing.graph) drawing.graph.remove();
-  makeAllNodesDraggable();
-}
-function setFrameType() {
-  const frameType = 'square'; //document.querySelector('[name=frame-type]:checked').value;
-  switch (frameType) {
-    case 'square':
-      drawing.knot && drawing.knot.remove();
-      drawing.frame && drawing.frame.remove();
-      drawSquareGrid();
-      drawing.frame && drawing.frame.draw();
-      drawing.knot && drawing.drawKnot();
-      break;
-      // TODO - ignoring "Type '"freeform"' is not comparable to type '"square"'" error here
-      // this happens because we're hardcoding the frameType value above
-      // so typescript knows it's never actually going to be anything but 'square'.
-      // When we get around to using the other frameType options again, we'll be able to remove
-      // this ts-ignore.
-      // @ts-ignore
-    case 'freeform':
-      startFreeformMode();
-      break;
-  }
 }
 
 function changeDrawingMode(newMode) {
@@ -78,5 +21,5 @@ document.addEventListener('DOMContentLoaded', () => {
       // throw error
     }
   });
-  setFrameType();
+  drawSquareGrid();
 }, false);
