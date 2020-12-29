e42435f13015048396193dcbe5e1a8642fbe754b
Joe Barnett <joebarnett876@gmail.com>
2020-12-29 20:29:24 +0000

    add todo about getting number of vertices for drawArrays

--- src/scripts/webgl/draw-webgl.ts
+++ src/scripts/webgl/draw-webgl.ts
@@ -10,10 +10,9 @@ let singlePixelLinesBuffer: WebGLBuffer;
 let linesBuffer: WebGLBuffer;
 let linesVAO: WebGLVertexArrayObject;
 let trianglesVAO: WebGLVertexArrayObject;
-// let uCanvasWidth: WebGLUniformLocation;
 
 const singlePixelLines: number[][] = [];
-let lines: number[] = [];
+let lines: number[][] = [];
 
 function setCanvasSize() {
   const canvas = gl.canvas;
@@ -46,13 +45,23 @@ export function draw() {
     gl.bindVertexArray(linesVAO);
     gl.bindBuffer(gl.ARRAY_BUFFER, singlePixelLinesBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(singlePixelLines), gl.STATIC_DRAW);
+    // TODO - get the number of vertices to draw by:
+    // - assuming we want to use the entirety of the data in the array
+    // - finding the set of active attributes (i.e. the enables attributes from the currently bound VAO)
+    // - for one of these attributes, find the length of the buffer it's attached to
+    // - divide the length of the buffer by the vertex size for that attribute
+    // - this should give you the number of vertices we are drawing, to pass into drawArrays.
+    // - you should get the same result regardless of which of the active attributes you choose from
+    // the current VAO.
+    // - it might be nice to check that you get the same result for all of the active attributes, and to
+    // throw an error, or just log a warning otherwise.
     gl.drawArrays(gl.LINES, 0, singlePixelLines.length * 2);
   }
   if (lines.length > 0) {
     gl.bindVertexArray(trianglesVAO);
     gl.bindBuffer(gl.ARRAY_BUFFER, linesBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(lines), gl.STATIC_DRAW);
-    gl.drawArrays(gl.TRIANGLES, 0, lines.length / 2);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(lines), gl.STATIC_DRAW);
+    gl.drawArrays(gl.TRIANGLES, 0, lines.length);
   }
   drawGrid();
 }
@@ -89,12 +98,12 @@ export function addLine({
   const norm = normal(lineVector([startX, startY], [endX, endY]));
   // if it's worth it we could later move part of this calculation into the vertex shader
   lines.push(
-    ...addVectors([startX, startY], scaleVector(norm, width / 2)),
-    ...addVectors([endX, endY], scaleVector(norm, -width / 2)),
-    ...addVectors([startX, startY], scaleVector(norm, -width / 2)),
-    ...addVectors([startX, startY], scaleVector(norm, width / 2)),
-    ...addVectors([endX, endY], scaleVector(norm, width / 2)),
-    ...addVectors([endX, endY], scaleVector(norm, -width / 2))
+    addVectors([startX, startY], scaleVector(norm, width / 2)),
+    addVectors([endX, endY], scaleVector(norm, -width / 2)),
+    addVectors([startX, startY], scaleVector(norm, -width / 2)),
+    addVectors([startX, startY], scaleVector(norm, width / 2)),
+    addVectors([endX, endY], scaleVector(norm, width / 2)),
+    addVectors([endX, endY], scaleVector(norm, -width / 2))
   );
 }
 
@@ -120,7 +129,7 @@ function setupAttribute(
   shaderVariableName: string,
   vao: WebGLVertexArrayObject,
   buffer: WebGLBuffer,
-  vertexSize: number,
+  arrayElementsPerVertex: number,
   dataType: number,
   normalized = false,
   stride = 0,
@@ -135,7 +144,7 @@ function setupAttribute(
   context.enableVertexAttribArray(attributeLocation);
   context.vertexAttribPointer(
     attributeLocation,
-    vertexSize,
+    arrayElementsPerVertex,
     dataType,
     normalized,
     stride,
@@ -152,6 +161,9 @@ export function start(context: OnscreenWebglContext) {
   trianglesVAO = createVAO(gl);
   linesBuffer = createBuffer(gl);
   singlePixelLinesBuffer = createBuffer(gl);
+  // We have two separate VAOs for drawing the single-pixel grid lines and the
+  // frame lines, which have some width. Both VAOs use the same shader attribute
+  // vPosition, but in each VAO the attribute is bound to a different buffer.
   setupAttribute(
     gl,
     program,
@@ -170,18 +182,4 @@ export function start(context: OnscreenWebglContext) {
     2,
     gl.FLOAT
   );
-  // const vPosition = gl.getAttribLocation(program, "vPosition");
-  // singlePixelLinesBuffer = createBuffer(gl);
-  // gl.bindBuffer(gl.ARRAY_BUFFER, singlePixelLinesBuffer);
-  // gl.bindVertexArray(linesVAO);
-  // // Load the data into the GPU
-  // // Associate our shader variables with our data buffer
-  // gl.enableVertexAttribArray(vPosition);
-  // gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-  // gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-  // const vPosition = gl.getAttribLocation(program, "vPosition");
-  // gl.bindBuffer(gl.ARRAY_BUFFER, linesBuffer);
-  // gl.bindVertexArray(trianglesVAO);
-  // gl.enableVertexAttribArray(vPosition);
-  // gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
 }
