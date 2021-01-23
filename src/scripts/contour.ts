fd8cce0fca7ece7e71a6bbb7d16c4e0efc63c12d
Joe Barnett <joebarnett876@gmail.com>
2021-01-23 12:21:20 +0000

    add big comment explaining contour maths

--- src/scripts/contour.ts
+++ src/scripts/contour.ts
@@ -1,10 +1,65 @@
-import numeric from "numeric";
+/*
+  Our goal here is to produce for each strand a set of
+  bezier curves which fit together in a manner thay complies
+  with some constraints.
 
+  We're using cubic beziers, so each curve is defined by 4 points: a
+  start point, two control points, and an end point.
+
+  Our constraints are given in terms of parametric continuity.
+  Some useful links for background:
+  https://people.eecs.berkeley.edu/~jfc/cs184f98/lec19/lec19.html
+  https://en.wikipedia.org/wiki/Smoothness#Geometric_continuity
+  https://stackoverflow.com/questions/12295773/joining-two-b%C3%A9zier-curves-smoothly-c2-continuous
+
+  Our first constraint is C0 continuity i.e. positional continuity;
+  this means the end point of each bezier curve is equal to the start
+  point of the next curve. This constraint doesn't have to be explicitly
+  applied as part of this calculation - it's ensured by the fact that
+  we're using the same points for the end of each curve as for the start
+  of the next one. This applies both for crossing-points and pointed-return
+  points.
+
+  At the crossing-points, we go beyond C0 continuity to C2 continuity.
+  This means that each pair of bezier curves meeting at a crossing-point
+  must be equal in both their first (C1) and second (C2) derivatives at
+  that point.
+
+  Note that since we're dealing with parametric curves, the derivatives
+  are vectors (and not generally unit vectors). When we say these derivatives
+  must be equal, we mean both in direction and magnitude.
+
+  The C1 and C2 constraints are applied only at the crossing-points. The
+  pointed-return points instead have a constant-angle constraint i.e. the
+  angle between the tangents of the two curves must equal some constant
+  POINTED_RETURN_THETA at the point where they meet.
+
+  This whole set of constraints, applied to a cyclic strand of crossing-points
+  and pointed-returns, produces a system of simultaneous equations which can
+  be expressed in matrix form:
+
+  Ax = B
+
+  where A is a matrix of coefficients (TODO explain size etc...), x is a vector containing the
+  x and y values of our control points which we want to find.
+
+  We solve this using the numeric library, which uses LU decomposition. This can be quite slow
+  and is something we should look into optimizing.
+
+  When creating large knots by dragging a frame on a square grid, you initially create a lot of
+  straight lines - one avenue worth exploring would be to account for these straight, lines first,
+  to reduce the amount of control points you need to solve for i.e. reduce the size of the matrices
+  involved in this calculation.
+
+  The matrices are always quite sparse - is the method used by numeric optimized for this?
+*/
+
+import numeric from "numeric";
 import Bezier from "./bezier/bezier.js";
 import { pointFollowing } from "./strand";
 import { Contour, Strand, Matrix, Polygon, Vector } from "./types";
 
-const theta = 1.5;
+const POINTED_RETURN_THETA = 1.5;
 
 /**
  * Take a basis strand (sequence of nodes), and add the actual beziers to it.
@@ -24,7 +79,6 @@ function bezier(polygon: Polygon): Bezier {
   return new Bezier(...polygon.reduce((arr, sub) => arr.concat(sub)));
 }
 
-// TODO - explain all the maths...
 function matrixSolution(strand: Strand) {
   const [matrix, equals] = constructMatrix(strand);
   const controlPoints = numeric.solve(matrix, equals);
@@ -33,6 +87,7 @@ function matrixSolution(strand: Strand) {
     yControlPoints: controlPoints.slice(controlPoints.length / 2),
   };
 }
+
 function getBezier(
   index: number,
   xControlPoints: number[],
@@ -96,7 +151,6 @@ function setC1continuity(
   return [newMatrix, newEquals];
 }
 
-// see https://stackoverflow.com/questions/12295773/joining-two-b%C3%A9zier-curves-smoothly-c2-continuous
 function setC2continuity(
   i: number,
   strand: Strand,
@@ -123,7 +177,10 @@ function setPointedReturnAngle(
   const newEquals = equals.slice();
   const strandElement = strand[i];
   const [x, y] = strandElement.point.coords;
-  const angle = strandElement.pr === "R" ? theta : 2 * Math.PI - theta;
+  const angle =
+    strandElement.pr === "R"
+      ? POINTED_RETURN_THETA
+      : 2 * Math.PI - POINTED_RETURN_THETA;
   const row1 = condition(2 * i - 1, [1, -Math.cos(angle)], strand);
   const row2 = condition(2 * i, [Math.sin(angle)], strand);
   const row3 = condition(2 * i, [-Math.sin(angle)], strand);
