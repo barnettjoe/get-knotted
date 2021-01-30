bfa2a33542292c140bd53a623862671cc55d0db3
Joe Barnett <joebarnett876@gmail.com>
2021-01-30 16:12:37 +0000

    started writing some test utils

--- src/c/matrix.c
+++ src/c/matrix.c
@@ -1,5 +1,27 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+void report_test_failure(char* text)
+
+{
+    printf(ANSI_COLOR_RED);
+    printf("test failed: ");
+    puts(text);
+    printf(ANSI_COLOR_RESET);
+}
+
+void report_test_success(char* text)
+{
+    printf(ANSI_COLOR_GREEN);
+    printf("test passed: ");
+    puts(text);
+    printf(ANSI_COLOR_RESET);
+}
 
 void* allocate_matrix(int rows, int cols)
 {
@@ -111,6 +133,17 @@ void solve_lup(
     // Obtain y by forward substitution
     // then obtain x by backward substitution
 }
+
+void expectToBeCloseTo(float actualVal, float expectedVal)
+{
+    float errorThreshold = 0.0005;
+    float diff = fabsf(actualVal - expectedVal);
+    if (diff > errorThreshold) {
+        report_test_failure("value was outside floating point error threshold");
+    }
+}
+
+void expectMatrixCloseTo() { }
 
 int matrix()
 {
